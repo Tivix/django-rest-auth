1cf6052a31657ffabe6d8a8fb3681bef4a8c3bbe
Mateusz Sikora <m.sikora@skytale.pl>
2014-05-05 17:57:47 +0200

    move definition of registration view and user profile model to runtest script

--- rest_auth/runtests.py
+++ rest_auth/runtests.py
@@ -6,6 +6,48 @@ sys.path.insert(0, test_dir)
 
 from django.test.utils import get_runner
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.sites.models import RequestSite
+from django.contrib.sites.models import Site
+from django.db import models
+
+from registration.models import RegistrationProfile
+from registration.backends.default.views import RegistrationView as BaseRegistrationView
+from registration import signals
+
+"""
+create user profile model
+"""
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    newsletter_subscribe = models.BooleanField(default=False)
+
+    class Meta:
+        app_label = 'rest_auth'
+
+settings.REST_PROFILE_MODULE = UserProfile
+
+
+"""
+overwrite register to avoid sending email
+"""
+class RegistrationView(BaseRegistrationView):
+    def register(self, request, **cleaned_data):
+        username, email, password = cleaned_data['username'], cleaned_data['email'], cleaned_data['password1']
+        if Site._meta.installed:
+            site = Site.objects.get_current()
+        else:
+            site = RequestSite(request)
+        new_user = RegistrationProfile.objects.create_inactive_user(username, email,
+                                                                    password, site, send_email=False)
+        signals.user_registered.send(sender=self.__class__,
+                                     user=new_user,
+                                     request=request)
+        return new_user
+
+settings.REST_REGISTRATION_BACKEND = 'rest_auth.runtests.RegistrationView'
+
+
 
 def runtests():
     TestRunner = get_runner(settings)
