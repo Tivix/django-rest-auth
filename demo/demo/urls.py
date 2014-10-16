81a96cfe19b40dda27ff5cc7e8e7d3b41d3d9412
Mateusz Sikora <m.sikora@skytale.pl>
2014-10-16 13:13:55 +0200

    final versio of demo project

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -4,11 +4,29 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
-    url(r'^signup/$', TemplateView.as_view(template_name="signup.html"), name='signup'),
-    url(r'^email-verification/$', TemplateView.as_view(template_name="email_verification.html"), name='email-verification'),
-    url(r'^login/$', TemplateView.as_view(template_name="login.html"), name='login'),
-    url(r'^password-reset/$', TemplateView.as_view(template_name="password_reset.html"), name='password-reset'),
+    url(r'^signup/$', TemplateView.as_view(template_name="signup.html"),
+        name='signup'),
+    url(r'^email-verification/$',
+        TemplateView.as_view(template_name="email_verification.html"),
+        name='email-verification'),
+    url(r'^login/$', TemplateView.as_view(template_name="login.html"),
+        name='login'),
+    url(r'^password-reset/$',
+        TemplateView.as_view(template_name="password_reset.html"),
+        name='password-reset'),
+    url(r'^password-reset/confirm/$',
+        TemplateView.as_view(template_name="password_reset_confirm.html"),
+        name='password-reset-confirm'),
 
+    url(r'^user-details/$',
+        TemplateView.as_view(template_name="user_details.html"),
+        name='user-details'),
+    url(r'^password-change/$',
+        TemplateView.as_view(template_name="password_change.html"),
+        name='password-change'),
+
+
+    # this url is used to generate email content
     url(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         TemplateView.as_view(template_name="password_reset_confirm.html"),
         name='password_reset_confirm'),
