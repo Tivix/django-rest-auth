be6b65dc555e7c4f3e262efda541fc1d1120808c
Marc Tudurí marctc@gmail.com <mtuduri@apsl.net>
2014-11-06 09:15:54 +0100

    modified utils.py in order to work properly with python2 and python3

--- rest_auth/utils.py
+++ rest_auth/utils.py
@@ -1,3 +1,4 @@
+from six import string_types
 from django.utils.importlib import import_module
 
 
@@ -5,7 +6,7 @@ def import_callable(path_or_callable):
     if hasattr(path_or_callable, '__call__'):
         return path_or_callable
     else:
-        assert isinstance(path_or_callable, (str, unicode))
+        assert isinstance(path_or_callable, string_types)
         package, attr = path_or_callable.rsplit('.', 1)
         return getattr(import_module(package), attr)
 
