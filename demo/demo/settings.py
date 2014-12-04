96002275e522b364c13dcca539e39705e8a6c1ef
Jonathan Batchelor <jmb@thebatchelors.net>
2014-12-04 17:12:58 +0000

    Some fixes for the demo app

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -105,3 +105,11 @@ SITE_ID = 1
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
