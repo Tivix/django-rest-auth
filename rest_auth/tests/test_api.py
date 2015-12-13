a93b7f5cec9873a6eb97a92f055306b11330548b
Poderyagin Egor <egor@crazyrussian.pro>
2015-12-13 22:24:27 +0300

    Added test case for reset by email in different case

--- rest_auth/tests/test_api.py
+++ rest_auth/tests/test_api.py
@@ -224,6 +224,15 @@ class APITestCase1(TestCase, BaseAPITestCase):
         }
         self.post(self.login_url, data=payload, status_code=200)
 
+    def test_password_reset_with_email_in_different_case(self):
+        user = get_user_model().objects.create_user(self.USERNAME, self.EMAIL.lower(), self.PASS)
+
+        # call password reset in upper case
+        mail_count = len(mail.outbox)
+        payload = {'email': self.EMAIL.upper()}
+        self.post(self.password_reset_url, data=payload, status_code=200)
+        self.assertEqual(len(mail.outbox), mail_count + 1)
+
     def test_password_reset_with_invalid_email(self):
         get_user_model().objects.create_user(self.USERNAME, self.EMAIL, self.PASS)
 
