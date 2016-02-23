aa839f97d218f8c84c7b4d8bf24b1fdd7eee245d
Maciej Jaworski <mjaworski@users.noreply.github.com>
2016-02-23 10:59:47 +0000

    Added missing import

--- rest_auth/registration/views.py
+++ rest_auth/registration/views.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
