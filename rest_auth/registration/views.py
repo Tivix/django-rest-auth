80adbae73a825e4a8749b5ec34272aa42f43faae
Girish <girishkumarkh@gmail.com>
2016-03-01 11:23:12 +0000

    Update views.py

--- rest_auth/registration/views.py
+++ rest_auth/registration/views.py
@@ -15,8 +15,7 @@ from allauth.account import app_settings as allauth_settings
 from rest_auth.app_settings import (TokenSerializer,
                                     JWTSerializer,
                                     create_token)
-from rest_auth.registration.serializers import (SocialLoginSerializer,
-                                                VerifyEmailSerializer)
+from rest_auth.registration.serializers import VerifyEmailSerializer
 from rest_auth.views import LoginView
 from rest_auth.models import TokenModel
 from .app_settings import (RegisterSerializer, 
