b926291fdcb1eb43bba8be4fee9af67652ebe715
Daniel Stanton <stringsonfire@me.com>
2017-01-24 13:56:23 +0000

    Replaced tabs with spaces

--- rest_auth/models.py
+++ rest_auth/models.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 
 if getattr(settings, 'REST_USE_KNOX', False):
-	try:
+    try:
         from knox.models import AuthToken as DefaultTokenModel
     except ImportError:
         raise ImportError("Install django-rest-knox to use REST_USE_KNOX = True")
 else:
-	from rest_framework.authtoken.models import Token as DefaultTokenModel
+    from rest_framework.authtoken.models import Token as DefaultTokenModel
 
 from .utils import import_callable
 
