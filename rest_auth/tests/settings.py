2059de79dbb9e697c617c4f07a3633547f30bf0c
Daniel Stanton <stringsonfire@me.com>
2017-01-25 18:03:23 +0000

    Installed Knox for tests

--- rest_auth/tests/settings.py
+++ rest_auth/tests/settings.py
@@ -88,7 +88,9 @@ INSTALLED_APPS = [
     'rest_auth',
     'rest_auth.registration',
 
-    'rest_framework_jwt'
+    'rest_framework_jwt',
+
+    'knox'
 ]
 
 SECRET_KEY = "38dh*skf8sjfhs287dh&^hd8&3hdg*j2&sd"
