f7cbbb084363edcbfaa6090d5c532ace77d530a2
Daniel Stanton <stringsonfire@me.com>
2017-01-25 13:11:05 +0000

    Added logoutall to main urls list

--- rest_auth/urls.py
+++ rest_auth/urls.py
@@ -14,12 +14,8 @@ urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='rest_login'),
     # URLs that require a user to be logged in with a valid session / token.
     url(r'^logout/$', LogoutView.as_view(), name='rest_logout'),
+    url(r'^logoutall/$' LogoutAllView.as_view(), name='rest_logout_all'),
     url(r'^user/$', UserDetailsView.as_view(), name='rest_user_details'),
     url(r'^password/change/$', PasswordChangeView.as_view(),
         name='rest_password_change'),
 ]
-
-if getattr(settings, 'REST_USE_KNOX', False):
-    urlpatterns.extend([
-        url(r'^logoutall/$' LogoutAllView.as_view(), name='rest_logout_all'),
-    ])
