703954f590dd45d3d218b3ac49e36987e3e95045
Usuario <kerycdiaz@gmail.com>
2017-09-24 21:08:16 -0500

    urls of allauth need

--- rest_auth/urls.py
+++ rest_auth/urls.py
@@ -5,7 +5,17 @@ from rest_auth.views import (
     PasswordResetView, PasswordResetConfirmView
 )
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view()
+def null_view(request):
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
 urlpatterns = [
+    url(r'^password-reset-confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', null_view ,name='password_reset_confirm'),
+    url(r"^confirm-email/$", null_view, name="account_email_verification_sent"),
     # URLs that do not require a session or valid token
     url(r'^password/reset/$', PasswordResetView.as_view(),
         name='rest_password_reset'),
