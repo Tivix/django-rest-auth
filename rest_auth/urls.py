3526d2ba0c6828f23acdb13922d347deb40347d2
Mariano Baragiola <mariano@baragiola.com.ar>
2017-12-12 11:32:25 -0300

    ADD app_name for django 2.0 compatibility

--- rest_auth/urls.py
+++ rest_auth/urls.py
@@ -5,6 +5,7 @@ from rest_auth.views import (
     PasswordResetView, PasswordResetConfirmView
 )
 
+app_name = 'rest_auth'
 urlpatterns = [
     # URLs that do not require a session or valid token
     url(r'^password/reset/$', PasswordResetView.as_view(),
