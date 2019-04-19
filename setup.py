555a510b58d37f9ccba3f3ef2ee80eda4a59b3c3
Ryan P Kilby <kilbyr@gmail.com>
2019-04-19 10:45:46 -0700

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -30,10 +30,21 @@ setup(
     },
     include_package_data=True,
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development'
     ],
 )
