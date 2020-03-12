597d17e6ff0f84d26d42808c6dd96427b64136c0
Mahmoud Adel <mahmoud.adel.mk@outlook.com>
2020-03-12 17:22:46 +0200

    Uncomment 'django.contrib.messages'
    When run  'python manage.py migrate --settings=demo.settings --noinput'
    
    I get this on console
    SystemCheckError: System check identified some issues:
    
    ERRORS:
    ?: (admin.E406) 'django.contrib.messages' must be in INSTALLED_APPS in order to use the admin application.
    
    So uncomment it to solve the issue.

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    # 'django.contrib.messages',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
 
