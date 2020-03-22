27815933f52ab393477abaa94bcae0d86fee6fcf
Michael <michaelmartinez@me.com>
2020-03-22 06:23:13 -0500

    Bumps minor version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ f.close()
 
 setup(
     name='dj-rest-auth',
-    version='0.1.4',
+    version='0.2.0',
     author='iMerica',
     author_email='imichael@pm.me',
     url='http://github.com/jazzband/dj-rest-auth',
