a190e036d2c1b4f6668e4bbe790175f51d4711b5
Michael <michaelmartinez@me.com>
2020-04-16 01:59:04 -0500

    Bug fixes related to Simple JWT integrations

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ f.close()
 
 setup(
     name='dj-rest-auth',
-    version='1.0.2',
+    version='1.0.3',
     author='iMerica',
     author_email='imichael@pm.me',
     url='http://github.com/jazzband/dj-rest-auth',
