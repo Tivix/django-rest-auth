ee3f3710e2f9d2b0812229fb2201e934ea874d8f
Michael <michaelmartinez@me.com>
2020-06-24 01:33:52 -0500

    Bumps version

--- dj_rest_auth/__version__.py
+++ dj_rest_auth/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'dj-rest-auth'
 __description__ = 'Authentication and Registration in Django Rest Framework.'
 __url__ = 'http://github.com/jazzband/dj-rest-auth'
-__version__ = '1.0.8'
+__version__ = '1.0.9'
 __author__ = '@iMerica https://github.com/iMerica'
 __author_email__ = 'imichael@pm.me'
 __license__ = 'MIT'
