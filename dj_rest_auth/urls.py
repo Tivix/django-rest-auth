32892f3177de396c52dc669c4caed82b68fdc175
Michael <michaelmartinez@me.com>
2020-08-09 13:59:28 -0500

    Fixes tests not running explicitly declared versions

--- dj_rest_auth/urls.py
+++ dj_rest_auth/urls.py
@@ -6,16 +6,13 @@ from django.conf import settings
 
 urlpatterns = [
     # URLs that do not require a session or valid token
-    path('password/reset/', PasswordResetView.as_view(),
-         name='rest_password_reset'),
-    path('password/reset/confirm/', PasswordResetConfirmView.as_view(),
-         name='rest_password_reset_confirm'),
+    path('password/reset/', PasswordResetView.as_view(), name='rest_password_reset'),
+    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
     path('login/', LoginView.as_view(), name='rest_login'),
     # URLs that require a user to be logged in with a valid session / token.
     path('logout/', LogoutView.as_view(), name='rest_logout'),
     path('user/', UserDetailsView.as_view(), name='rest_user_details'),
-    path('password/change/', PasswordChangeView.as_view(),
-         name='rest_password_change'),
+    path('password/change/', PasswordChangeView.as_view(), name='rest_password_change'),
 ]
 
 if getattr(settings, 'REST_USE_JWT', False):
